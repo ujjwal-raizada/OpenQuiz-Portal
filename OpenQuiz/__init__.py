c66cd7746231540b3b23afccb39d10f7daf17c54
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-04-15 00:22:44 +0530

    OpenQuiz: convert folder into module.

---
+++ OpenQuiz/__init__.py
@@ -0,0 +1,9 @@
+# OpenQuiz Package
+
+# This module contains all the database operations
+# for OpenQuiz portal, it can easily be imported
+# and used.
+
+# Written from scratch, so no dependencies.
+
+__all__ = ['student', 'faculty', 'course', 'quiz']
