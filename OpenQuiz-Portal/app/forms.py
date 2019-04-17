3cfd81b037ee6393931ce3df38a74ad902b96541
Daksh Yashlaha <dyashlaha039909@gmail.com>
2019-04-17 15:58:18 +0530

    Flask: Add Course Registration route

--- OpenQuiz-Portal/app/forms.py
+++ OpenQuiz-Portal/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
@@ -9,12 +9,14 @@ class LoginForm(FlaskForm):
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
 
-class RegistrationForm(FlaskForm):
+class RegistrationFormStudent(FlaskForm):
   username = StringField('Username', validators = [DataRequired()])
   email = StringField('Email', validators = [DataRequired(), Email()])
   password = PasswordField('Password', validators = [DataRequired()])
   password2 = PasswordField(
     'Repeat Password', validators = [DataRequired(), EqualTo('password')])
+  student_id = StringField('College ID', validators = [DataRequired()])
+  student_branch = StringField('Branch', validators = [DataRequired()])
   submit = SubmitField('Register')
 
   def validate_username(self, username):
@@ -26,3 +28,38 @@ class RegistrationForm(FlaskForm):
     user = User.query.filter_by(email = email.data).first()
     if user is not None:
       raise ValidationError('Please use a different email address')
+
+class RegistrationFormFaculty(FlaskForm):
+  username = StringField('Username', validators = [DataRequired()])
+  email = StringField('Email', validators = [DataRequired(), Email()])
+  password = PasswordField('Password', validators = [DataRequired()])
+  password2 = PasswordField(
+    'Repeat Password', validators = [DataRequired(), EqualTo('password')])
+  department = StringField('Department', validators = [DataRequired()])
+  submit = SubmitField('Register')
+
+  def validate_username(self, username):
+    user = User.query.filter_by(username = username.data).first()
+    if user is not None:
+      raise ValidationError('Please use a different username')
+
+  def validate_email(self, email):
+    user = User.query.filter_by(email = email.data).first()
+    if user is not None:
+      raise ValidationError('Please use a different email address')
+
+class CourseForm(FlaskForm):
+  course_id = StringField('Enter Course ID', validators = [DataRequired()])
+  course_name = StringField('Enter Course Name', validators = [DataRequired()])
+  ic_id = StringField('Enter IC', validators = [DataRequired()])
+  submit = SubmitField('Add Course')
+
+class StudentCourseForm(FlaskForm):
+  student_id = StringField('Enter Student ID', validators = [DataRequired()])
+  course = StringField('Enter Course', validators = [DataRequired()])
+  submit = SubmitField('Enroll')
+
+class FacultyCourseForm(FlaskForm):
+  faculty_email = StringField('Enter Faculty Email', validators = [DataRequired()])
+  course = StringField('Enter Course', validators = [DataRequired()])
+  submit = SubmitField('Enroll')
