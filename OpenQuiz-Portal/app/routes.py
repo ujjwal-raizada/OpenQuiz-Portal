287f5cb2f4970a46366dba6ceaab108a5485bcb3
Prakhar Goenka <prakhar.goinka@gmail.com>
2019-04-23 02:35:15 +0530

    Add quiz form.

--- OpenQuiz-Portal/app/routes.py
+++ OpenQuiz-Portal/app/routes.py
@@ -112,77 +112,102 @@ def facultycourse():
 
 @app.route('/createquiz', methods=['GET', 'POST'])
 @login_required
-def createquiz():    
+def createquiz():
+
   faculty_id = Faculty.get_faculty_id(current_user.email)[1]
   courses = Faculty.get_faculty_course(faculty_id)
+
   form = CreateQuiz()
   form.course.choices = [(course['cid'], course['cid']) for course in courses]
-  if form.validate_on_submit():
-    #course = Course.get_all_course()[0][0]
-    if (False):#validate course name
-      flash('__ is not a valid course name')
-    else:
-      print(Quiz.create_quiz(faculty_id, form.course.data, form.quiz_name.data, form.start_time.data, form.end_time.data))
-      flash('quiz created successfully')
-      return redirect(url_for('index'))
+
+  if form.validate_on_submit():    
+    Quiz.create_quiz(faculty_id, form.course.data, form.quiz_name.data,
+     form.start_time.data, form.end_time.data)
+    flash('Quiz created successfully')
+    return redirect(url_for('index'))
+
   return render_template('createQuiz.html', title = 'Create Quiz', form = form)
 
 @app.route('/createproblem', methods=['GET', 'POST'])
 @login_required
 def createproblem():
+
   faculty_id = Faculty.get_faculty_id(current_user.email)[1]
   quizes = Quiz.get_faculty_quiz(faculty_id)
-  print(quizes)
+
   form = CreateProblem()
-  form.quiz_id.choices = [(str(quiz['qid']), "{}: {}".format(quiz['cid'], quiz['qname'])) for quiz in quizes]
+  form.quiz_id.choices = [(str(quiz['qid']), "{}: {}".format(quiz['cid'], quiz['qname']))\
+   for quiz in quizes]
+
   if form.validate_on_submit():
-    Quiz.create_problem(form.quiz_id.data,form.statement.data, form.op_1.data, form.op_2.data, form.op_3.data, form.op_4.data, form.ans.data, form.positive.data, form.negative.data)
+    Quiz.create_problem(form.quiz_id.data,form.statement.data, form.op_1.data, form.op_2.data,
+     form.op_3.data, form.op_4.data, form.ans.data, form.positive.data, form.negative.data)
     flash('problem created successfully')
     return redirect(url_for('index'))
+
   return render_template('createProblem.html', title = 'Create Problem', form = form)
 
 @app.route('/getproblems', methods=['GET', 'POST'])
 @login_required
 def getproblems():
+
   faculty_id = Faculty.get_faculty_id(current_user.email)[1]
   quizes = Quiz.get_faculty_quiz(faculty_id)
-  print(quizes)
+
   form = GetProblems()
-  form.quiz_id.choices = [(str(quiz['qid']), "{}: {}".format(quiz['cid'], quiz['qname'])) for quiz in quizes]
+  form.quiz_id.choices = [(str(quiz['qid']), "{}: {}".format(quiz['cid'], quiz['qname']))\
+   for quiz in quizes]
+
   if form.validate_on_submit():
-    if True:  # check if quiz id is valid
-      problems = Quiz.get_problems(form.quiz_id.data)
-      return render_template('problemsList.html', title='Hme', quiz_id = form.quiz_id.data, problems = problems)
-    else:
-      flash('Invalid Quiz id')
+    quiz_id = form.quiz_id.data
+    problems = Quiz.get_problems(quiz_id)
+    return render_template('problemsList.html', title='Problems of Quiz' + quiz_id,
+     quiz_id = quiz_id, problems = problems)
+
   return render_template('getProblems.html', title =  'Get Problems', form = form)
 
 @app.route('/enterquiz', methods=['GET', 'POST'])
 @login_required
 def enterquiz():
+
   quizes = Quiz.get_all_quiz()
+
   form = GetProblems()
-  form.quiz_id.choices = [(quiz[0], quiz[0]) for quiz in quizes]
+  form.quiz_id.choices = [(str(quiz[0]), quiz[3]) for quiz in quizes]
+
   if form.validate_on_submit():
     return redirect (url_for('quizform', quiz_id=form.quiz_id.data))
-  return render_template('getProblems.html', title =  'Get Problems', form = form)
+
+  return render_template('getProblems.html', title =  'Enter Quiz', form = form)
 
 @app.route('/quiz/<quiz_id>', methods=['GET'])
 @login_required
 def quizform(quiz_id):
+
   student_id = current_user.user_id
-  if (Quiz.is_student_in_quiz(student_id,quiz_id) == False):#check if quiz id is valid
+  if (Quiz.is_student_in_quiz(student_id,quiz_id) == False):  # check if quiz id is valid
      flash('Sorry you have not enrolled for this Quiz!!' )
      return redirect (url_for('enterquiz'))
-  #problems = Quiz.get_problems(quiz_id);
-  problems = [{"pid": 1,"statement": "Number of edges in a tree","qid": 1, "option1": "n", "option2": "nn", "option3": "3n", "option4": "2n"},
-   {"pid": 2,"statement": " 2Number2 of edges in a tree","qid": 1, "option1": "n", "option2": "nn", "option3": "3n", "option4": "2n"},
-   {"pid": 3,"statement": " 3Number3 of edges in a tree","qid": 1, "option1": "n", "option2": "nn", "option3": "3n", "option4": "2n"}]  
-  return render_template('quiz.html', title =  'Quiz', problems=problems, quiz_id = quiz_id)
+
+  problems = (Quiz.get_problems(quiz_id))
+  return render_template('quiz.html', title = 'Quiz' + quiz_id, problems=problems,
+   quiz_id = quiz_id)
 
 @app.route('/quiz/<quiz_id>', methods=['POST'])
 @login_required
 def quizforrm(quiz_id):
+
   d = request.form.to_dict()
-  print(d)
+  response_list  = [(int(pid),d[pid]) for pid in d]
+
+  student_id = current_user.user_id
+  Quiz.insert_response(quiz_id, student_id, response_list)
+  
+  flash("Successfully submitted your responses")
   return redirect('/')
+  # submission_status = Quiz.insert_response(quiz_id, student_id, response_list)
+  # if(submission_status == True):
+  #   flash("Successfully submitted your responses")
+  #   return redirect('/')
+  # else:
+  #   flash("Sorry could not submit your responses")
