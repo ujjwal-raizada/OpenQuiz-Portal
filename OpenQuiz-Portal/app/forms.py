b0025f2966ce15a87c056d8bfebeeb779c0d8cba
Prakhar Goenka <prakhar.goinka@gmail.com>
2019-04-24 12:56:41 +0530

    Add validation for attempting quiz and refactor routes/forms.

--- OpenQuiz-Portal/app/forms.py
+++ OpenQuiz-Portal/app/forms.py
@@ -13,7 +13,7 @@ class LoginForm(FlaskForm):
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
 
-class RegistrationFormStudent(FlaskForm):
+class RegistrationFormStudent(FlaskForm):  
   branch = [('B.E. –Chemical Engineering', 'B.E. –Chemical Engineering'), 
             ('B.E. –Civil', 'B.E. –Civil'), 
             ('B.E. –Computer Science', 'B.E. –Computer Science'), 
@@ -27,30 +27,35 @@ class RegistrationFormStudent(FlaskForm):
             ('M.Sc. –Economics', 'M.Sc. –Economics'),
             ('M.Sc. –Mathematics', 'M.Sc. –Mathematics'),
             ('M.Sc. –Physics', 'M.Sc. –Physics')]
+
   username = StringField('Username', validators = [DataRequired()])
   name = StringField('Full Name', validators = [DataRequired()])
   email = StringField('Email', validators = [DataRequired(), Email()])
   password = PasswordField('Password', validators = [DataRequired()])
-  password2 = PasswordField(
-    'Repeat Password', validators = [DataRequired(), EqualTo('password')])
+  password2 = PasswordField('Repeat Password', validators = [DataRequired(),
+   EqualTo('password')])
   studentid = StringField('College ID', validators = [DataRequired()])
   studentbranch = SelectField('Branch', choices = branch, validators = [DataRequired()])
   submit = SubmitField('Register')
 
   def validate_username(self, username):
+
     user = User.query.filter_by(username = username.data).first()
     if user is not None:
       raise ValidationError('Please use a different username')
 
   def validate_email(self, email):
+
     user = User.query.filter_by(email = email.data).first()
     if user is not None:
       raise ValidationError('Please use a different email address')
 
   def validate_studentid(self, studentid):
+
     temp = Student.is_student(studentid.data)
     if(temp):
       raise ValidationError('Entered ID is already registered')
+
 
 class RegistrationFormFaculty(FlaskForm):
   department = [('Dept. of Chemical Engineering', 'Dept. of Chemical Engineering'),
@@ -64,13 +69,15 @@ class RegistrationFormFaculty(FlaskForm):
                 ('Dept. of Economics', 'Dept. of Economics'),
                 ('Dept. of Physics', 'Dept. of Physics'),
                 ('Dept. of Mathematics', 'Dept. of Mathematics')]
+
   username = StringField('Username', validators = [DataRequired()])
   name = StringField('Full Name', validators = [DataRequired()])
   email = StringField('Email', validators = [DataRequired(), Email()])
   password = PasswordField('Password', validators = [DataRequired()])
-  password2 = PasswordField(
-    'Repeat Password', validators = [DataRequired(), EqualTo('password')])
-  department = SelectField('Department', choices = department, validators = [DataRequired()])
+  password2 = PasswordField('Repeat Password', validators = [DataRequired(),
+   EqualTo('password')])
+  department = SelectField('Department', choices = department,
+   validators = [DataRequired()])
   submit = SubmitField('Register')
 
   def validate_username(self, username):
@@ -79,17 +86,20 @@ class RegistrationFormFaculty(FlaskForm):
       raise ValidationError('Please use a different username')
 
   def validate_email(self, email):
+
     user = User.query.filter_by(email = email.data).first()
     if user is not None:
       raise ValidationError('Please use a different email address')
+
     id = Faculty.get_faculty_id(email.data)
     if(id[0]):
-      temp = Faculty.is_faculty(id[1])
+      temp = Faculty.is_faculty(id[1])      
       if(temp):
         raise ValidationError('Entered email is already registered')
 
 
 class CourseForm(FlaskForm):
+
   course_id = StringField('Enter Course ID', validators = [DataRequired()])
   course_name = StringField('Enter Course Name', validators = [DataRequired()])
   ic_id = SelectField('Select IC', validators = [DataRequired()])
@@ -100,7 +110,9 @@ class CourseForm(FlaskForm):
     if(temp):
       raise ValidationError('Entered Course is already registered')
 
+
 class StudentCourseForm(FlaskForm):
+
   course = SelectField('Select Course', validators = [DataRequired()])
   submit = SubmitField('Enroll')
 
@@ -109,7 +121,9 @@ class StudentCourseForm(FlaskForm):
     if(not temp):
       raise ValidationError('The entered ID is not registered')
 
+
 class FacultyCourseForm(FlaskForm):  
+
   course = SelectField('Select Course', validators = [DataRequired()])
   submit = SubmitField('Enroll')
 
@@ -118,32 +132,42 @@ class FacultyCourseForm(FlaskForm):
     if(not temp):
       raise ValidationError('The entered email is not registered')
 
+
 class CreateQuiz(FlaskForm):
+
   course = SelectField('Enter Course', validators = [DataRequired()])
   quiz_name = StringField('Enter Quiz Name', validators = [DataRequired()])
-  start_time = StringField('Enter Quiz Start Time (format: Year Month Day Hour Min Sec)')
-  end_time = StringField('Enter Quiz End Time')
+  start_time = StringField('Starts At:(YYYY MM DD HH MM SS)', validators = [DataRequired()])  
+  end_time = StringField('Ends At: (YYYY MM DD HH MM SS)', validators = [DataRequired()])
   submit = SubmitField('Create Quiz')
 
+
 class CreateProblem(FlaskForm):
+
   quiz_id = SelectField('Select Quiz', validators = [DataRequired()])
   statement = StringField('Question Statement', validators = [DataRequired()])
   op_1 = StringField('Option 1', validators = [DataRequired()])
   op_2 = StringField('Option 2', validators = [DataRequired()])
   op_3 = StringField('Option 3', validators = [DataRequired()])
   op_4 = StringField('Option 4', validators = [DataRequired()])
-  ans = SelectField('Answer', choices = [('A','Option 1'), ('B','Option 2'), ('C','Option 3'), ('D','Option 4')], validators = [DataRequired()])
-  positive = IntegerField('Marks awarded for correct response', validators = [DataRequired()])
-  negative = IntegerField('Marks deducted for incorrect response', validators = [DataRequired()])
+  ans = SelectField('Answer', choices = [('A','Option 1'), ('B','Option 2'),
+   ('C','Option 3'), ('D','Option 4')], validators = [DataRequired()])
+  positive = IntegerField('Marks awarded for correct response',
+   validators = [DataRequired()])
+  negative = IntegerField('Marks deducted for incorrect response',
+   validators = [DataRequired()])
   submit = SubmitField('Create Problem')
+
 
 class GetProblems(FlaskForm):
   quiz_id = SelectField('Select Quiz', validators = [DataRequired()])
   submit = SubmitField('Get Problems')
 
+
 class QuizForm(FlaskForm):
   ans = StringField('Your response')
   submit = SubmitField('dont')
+
 
 class GetResult(FlaskForm):
   quiz_id = SelectField('Select Quiz', validators = [DataRequired()])
--- OpenQuiz-Portal/app/routes.py
+++ OpenQuiz-Portal/app/routes.py
@@ -17,102 +17,132 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
+
   print(os.getcwd()) 
   return render_template('index.html', title='Home', user_type = current_user.user_type)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
   if current_user.is_authenticated:
     return redirect(url_for('index'))
+
   form = LoginForm()
+
   if form.validate_on_submit():
     user = User.query.filter_by(email = form.email.data).first()
+
     if user is None or not user.check_password(form.password.data):
       flash('Invalid email or password')
       return redirect(url_for('login'))
+
     login_user(user, remember = form.remember_me.data)
     next_page = request.args.get('next')
+
     if not next_page or url_parse(next_page).netloc != '':
       next_page = url_for('index')
     return redirect(next_page)
+
   return render_template('login.html', title = 'Sign In', form = form)
 
 @app.route('/logout')
 def logout():
+
   logout_user()
   return redirect(url_for('index'))
 
 @app.route('/register/student', methods=['GET', 'POST'])
 def registerStudent():
+
   if current_user.is_authenticated:
     return redirect(url_for('index'))
+
   form = RegistrationFormStudent()
+
   if form.validate_on_submit():
-    print(Student.create_student(form.studentid.data, form.name.data))
+    Student.create_student(form.studentid.data, form.name.data)
     user = User(username = form.username.data, email = form.email.data, 
                 user_id = form.studentid.data, user_type = 'Student')
     user.set_password(form.password.data)
+
     db.session.add(user)
     db.session.commit()
-    flash('Congratulations, you are now a registered student!')
+    flash('Congratulations, you are now a registered student!')    
     return redirect(url_for('login'))
+
   return render_template('registerStudent.html', title = 'Register Student', form = form)
 
 @app.route('/register/faculty', methods=['GET', 'POST'])
 def registerFaculty():
+
   if current_user.is_authenticated:
     return redirect(url_for('index'))
+
   form = RegistrationFormFaculty()
+
   if form.validate_on_submit():
     print(Faculty.create_faculty(form.name.data, form.email.data, form.department.data))
     user = User(username = form.username.data, email = form.email.data, 
                 user_id = 'Null', user_type = 'Faculty')
     user.set_password(form.password.data)
+
     db.session.add(user)
     db.session.commit()
     flash('Congratulations, you are now a registered faculty!')
     return redirect(url_for('login'))
+
   return render_template('registerFaculty.html', title = 'Register Faculty', form = form)
 
 @app.route('/addcourse', methods=['GET', 'POST'])
 @login_required
 def addcourse():
+
   form = CourseForm()
   form.ic_id.choices = [(str(i[0]), i[1]) for i in Faculty.get_all_faculty()]
+
   if form.validate_on_submit():
     Course.create_course(form.course_id.data, form.course_name.data, int(form.ic_id.data)) 
     flash('The course has been added successfully')
     return redirect(url_for('index'))
+
   return render_template('courseForm.html', title = 'Add Course', form = form)
 
 @app.route('/studentcourse', methods=['GET', 'POST'])
 @login_required
 def studentcourse():
+
   form = StudentCourseForm()
   form.course.choices = [(course[0], course[1]) for course in Course.get_all_course()]
+
   if form.validate_on_submit():
     temp = Course.is_Student_in_course(current_user.user_id, form.course.data)
+
     if temp:
       flash('The student is already enrolled in the course')
     else:
       Course.insert_student_in_course(current_user.user_id, form.course.data)
       flash('The student has been successfully enrolled in the course')
     return redirect(url_for('index'))
+
   return render_template('studentCourse.html', title = 'Enroll Course', form = form)
 
 @app.route('/facultycourse', methods=['GET', 'POST'])
 @login_required
 def facultycourse():
+
   form = FacultyCourseForm()
   form.course.choices = [(course[0], course[1]) for course in Course.get_all_course()] 
+
   if form.validate_on_submit():
     faculty_id = (Faculty.get_faculty_id(current_user.email))
+
     if faculty_id[0] == False:
       flash('Error: Faculty not registered in the database')
     else:
       Course.insert_faculty_in_course(faculty_id[1], form.course.data)
       flash('The faculty has been successfully enrolled in the course')
       return redirect(url_for('index'))
+      
   return render_template('facultyCourse.html', title = 'Enroll Course', form = form)
 
 @app.route('/createquiz', methods=['GET', 'POST'])
@@ -191,50 +221,51 @@ def enterquiz():
   form.quiz_id.choices = [(str(quiz[0]), quiz[3]) for quiz in student_quizzes]
 
   if form.validate_on_submit():
-    print(Quiz.quiz_attempted(form.quiz_id.data, current_user.user_id))
-
-    if not Quiz.quiz_status(form.quiz_id.data)[0]:
-      flash(Quiz.quiz_status(form.quiz_id.data)[1])
-
-    if Quiz.quiz_attempted(form.quiz_id.data, current_user.user_id):
-      flash('You have already attempted the quiz')
-      return redirect(url_for('index'))
-    else:
-      return redirect (url_for('quizform', quiz_id=form.quiz_id.data))
+    return redirect (url_for('quizformget', quiz_id=form.quiz_id.data))
 
   return render_template('getProblems.html', title =  'Enter Quiz', form = form)
 
 @app.route('/quiz/<quiz_id>', methods=['GET'])
 @login_required
-def quizform(quiz_id):
+def quizformget(quiz_id):
 
-  # student_id = current_user.user_id
-  # if (Quiz.is_student_in_quiz(student_id,quiz_id) == False):  # check if quiz id is valid
-  #    flash('Sorry you have not enrolled for this Quiz!!' )
-  #    return redirect (url_for('enterquiz'))
+  student_id = current_user.user_id
+  if not Quiz.is_student_in_quiz(student_id,quiz_id):
+     flash('Sorry you have not enrolled for this Quiz!!' )
+     return redirect (url_for('enterquiz'))
 
+  if not Quiz.quiz_status(quiz_id)[0]:
+    flash(Quiz.quiz_status(quiz_id)[1])
+    return redirect(url_for('enterquiz'))
+
+  if Quiz.quiz_attempted(quiz_id, student_id):
+      flash('You have already attempted the quiz')
+      return redirect(url_for('index'))
+  
   problems = (Quiz.get_problems(quiz_id))
-  return render_template('quiz.html', title = 'Quiz' + quiz_id, problems=problems,
+  return render_template('quiz.html', title = 'Quiz' + quiz_id, problems = problems,
    quiz_id = quiz_id)
 
 @app.route('/quiz/<quiz_id>', methods=['POST'])
 @login_required
-def quizforrm(quiz_id): 
+def quizformpost(quiz_id): 
+
+  if not Quiz.quiz_status(quiz_id)[0]:
+    flash(Quiz.quiz_status(quiz_id)[1])
+    return redirect(url_for('index'))
 
   d = request.form.to_dict()
   response_list  = [(int(pid),d[pid]) for pid in d]
 
   student_id = current_user.user_id
-  Quiz.insert_response(quiz_id, student_id, response_list)
-  flash("Successfully submitted your responses")
+  submission_status = Quiz.insert_response(quiz_id, student_id, response_list)
 
-  return redirect('/')
-  # submission_status = Quiz.insert_response(quiz_id, student_id, response_list)
-  # if(submission_status == True):
-  #   flash("Successfully submitted your responses")
-  #   return redirect('/')
-  # else:
-  #   flash("Sorry could not submit your responses")
+  if(submission_status == True):
+    flash("Successfully submitted your responses")
+    return redirect('/')
+  else:
+    flash("Sorry could not submit your responses")
+    return redirect (url_for('quizform', quiz_id=quiz_id))
 
 @app.route('/quiz/result', methods = ['GET', 'POST'])
 @login_required
@@ -251,6 +282,6 @@ def generateresult():
     results = Quiz.generate_mark_list(form.quiz_id.data)
     print(results)
     return render_template('results.html', title = 'Results for quiz ' + form.quiz_id.data, 
-                            form = form, results = results)
+                            form = form, results = results, quiz_id = form.quiz_id.data)
 
   return render_template('getResult.html', title =  'Select Quiz', form = form)
